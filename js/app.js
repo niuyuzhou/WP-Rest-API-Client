1ccbccd7dc5f542fee3c2d9e83af6a21df469e65
tom.niu <tom.niu8@gmail.com>
2016-05-11 20:24:34 +1000

    JsFormat

--- js/app.js
+++ js/app.js
@@ -1,30 +1,38 @@
-(function(){
+(function() {
 	var myApp = angular.module('myApp', ['ngRoute', 'WPSerivce', 'Ctrls', 'Directives']);
 
 	myApp.config(['WPConfig',
-		function(WPConfig){
+		function(WPConfig) {
 			WPConfig.baseURL = 'http://localhost/wordpress/';
 			WPConfig.partials = 'partials/';
-	}]);
-	
+		}
+	]);
+
 	myApp.config(['$routeProvider', 'WPConfig',
-		function($routeProvider, WPConfig){
+		function($routeProvider, WPConfig) {
 			$routeProvider
 				.when('/', {
 					templateUrl: WPConfig.partials + 'main.html',
 					controller: 'Main'
 				})
-				.when('/Login',{
+				.when('/Login', {
 					templateUrl: WPConfig.partials + 'login.html',
 					controller: 'Login'
 				})
-				.when('/Logout',{
+				.when('/Logout', {
 					templateUrl: WPConfig.partials + 'logout.html',
 					controller: 'Logout'
 				})
 				.otherwise({
 					templateUrl: WPConfig.partials + '404.html',
 				});
-	}]);
-	
-})()
+		}
+	]);
+
+	myApp.config(['$httpProvider',
+		function($httpProvider) {
+			$httpProvider.interceptors.push('basicAuthInterceptor');
+		}
+	]);
+
+})();
